d046c0d3d8dd46898d657fa34828ee890394326d
Fatelvleh <ai.fatemeh.jamali@gmail.com>
2022-05-08 14:59:33 +0430

    Add project files.

---
+++ FirstApp/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace FirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            resources.ApplyResources(this.lblName, "lblName");
+            this.lblName.Name = "lblName";
+            // 
+            // btnSayHello
+            // 
+            resources.ApplyResources(this.btnSayHello, "btnSayHello");
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // txtBox
+            // 
+            resources.ApplyResources(this.txtBox, "txtBox");
+            this.txtBox.Name = "txtBox";
+            // 
+            // lblHello
+            // 
+            resources.ApplyResources(this.lblHello, "lblHello");
+            this.lblHello.Name = "lblHello";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label lblHello;
+    }
+}
+
---
+++ FirstApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+            if (txtBox.Text != "")
+            {
+                lblHello.ForeColor = Color.Green;
+                lblHello.Text = "سلام" + " " + txtBox.Text;
+            }
+            else
+            {
+                lblHello.ForeColor = Color.Red;
+                lblHello.Text = "لطفا نام خود را وارد کنید";
+            }
+
+        }
+
+       
+    }
+}
